bcd5ac658f8200477e68e1036279e88f7415308e
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-03 21:52:05 +0800

    create dice componenet and add font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Dice from './dice'
 
 function App() {
   return (
    
     <div className="App">
-      <div className="inner-box"></div>
+      <div className="inner-box">
+        <div className="dice-container">
+          <Dice/>
+          <Dice/>
+          <Dice/>
+          <Dice/>
+          <Dice/>
+          <Dice/>
+          <Dice/>
+          <Dice/>
+          <Dice/>
+          <Dice/>
+        
+        </div>
+      
+      </div>
     </div>
   
   );
---
+++ src/dice.js
@@ -0,0 +1,13 @@
+
+function Dice () {
+
+
+return (
+    <div className='die'>
+        1
+    </div>
+
+    )
+}
+
+export default Dice
