1577d9f43d26679514d861ca313af4a8771fb0e5
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-04 13:18:08 +0800

    set useState for number on die
    -import useState and useEffect because I know I will need useEffect later
    
    -create funciton that generates  an array of ten random numebrs
    
    -create value diceNum that sets the value of each <Dice/> compoentnet.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './styling/App.css';
 import Dice from './componets/dice'
+import React, {useState, useEffect} from 'react'
 
 function App() {
+
+//state that sets the array generated in allNewDice to state of dieValue
+const [dieValue, setDieValue] = useState(allNewDice());
+
+function allNewDice(){
+ 
+ let values=[]
+
+  for(let i=0; i<10; i++){
+    values.push(Math.ceil(Math.random()*6))
+  }
+
+  return values
+}
+
+// map through dieValue (array of nums generated in allNewDice) and sets the vale of Dice to that number.
+let diceNum = dieValue.map(num =>
+  
+  <Dice 
+    
+    value= {num}
+  />
+)
+
+  
+
+
   return (
     <div className="App">
       <div className="inner-box">
         <div className="dice-container">
-          <Dice value="2" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="3" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="6" />
-          <Dice value="1" />
+          {/* calles diceNum to get a dice component and render it.
+          since there is only ten numebrs it will render 10x */}
+          {diceNum}
         </div>
+        <button className='roll-btn'>Roll</button>
       </div>
     </div>
   );
