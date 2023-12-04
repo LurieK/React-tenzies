bb461adbf6519cad12541e8715cd93c52fa1e449
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-04 11:54:23 +0800

    change dice background color to pop them out

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
     <div className="App">
       <div className="inner-box">
         <div className="dice-container">
+          <Dice value="2" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="3" />
           <Dice value="1" />
           <Dice value="1" />
           <Dice value="1" />
           <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
+          <Dice value="6" />
           <Dice value="1" />
         </div>
       </div>
